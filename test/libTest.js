6d3c753c4981460cb87272dc8cfadbfc54aa3f11
Bhawna Sharma <shbhawna@In-shbhawna.local>
2018-11-21 17:04:13 +0530

    Removed tests of mapper functions

--- test/libTest.js
+++ test/libTest.js
@@ -25,29 +25,6 @@ const isOdd = function(number) {
   return number % 2;
 }
 
-
-describe("increment", function(){ 
-  it("should increment positive number", function(){
-    assert.equal(increment(1), 2);
-    assert.equal(increment(4), 5);
-  });
-  it("should increment negative number", function(){
-    assert.equal(increment(-1), 0);
-    assert.equal(increment(-4), -3);
-  });
-});
-
-describe("decrement", function(){
-  it("should decrement positive number", function(){
-    assert.equal(decrement(1), 0);
-    assert.equal(decrement(4), 3);
-  });
-  it("should decrement negative number", function(){
-    assert.equal(decrement(-1), -2);
-    assert.equal(decrement(-4), -5);
-  });
-});
-
 describe("map", function(){
   it("of empty array should return empty array", function(){
       assert.deepEqual(map([], increment), []);
