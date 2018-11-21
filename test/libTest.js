53ba81455e663f73741d3db233a991ff5f07bd7e
Bhawna Sharma <shbhawna@In-shbhawna.local>
2018-11-21 17:01:29 +0530

    Changed discription of tests

--- test/libTest.js
+++ test/libTest.js
@@ -49,59 +49,49 @@ describe("decrement", function(){
 });
 
 describe("map", function(){
-  describe("with increment", function(){
-    it("should increment all array elements", function(){
+  it("of empty array should return empty array", function(){
       assert.deepEqual(map([], increment), []);
-      assert.deepEqual(map([0], increment), [1]);
+      assert.deepEqual(map([], decrement), []);
+      assert.deepEqual(map([], square), []);
+      assert.deepEqual(map([], cube), []);
+    });
+  it("of array of arity 1 should return array of arity 1(containing mapped value of that element)", function(){
       assert.deepEqual(map([1], increment), [2]);
-      assert.deepEqual(map([-1], increment), [0]);
-      assert.deepEqual(map([1,2], increment), [2,3]);
+      assert.deepEqual(map([1], decrement), [0]);
+      assert.deepEqual(map([1], square), [1]);
+      assert.deepEqual(map([1], cube), [1]);
+    });
+  it("of array of any arity should return array of same arity(containing mapped value element of all elements)", function(){
       assert.deepEqual(map([-1,-2], increment), [0,-1]);
       assert.deepEqual(map([1,0,2], increment), [2,1,3]);
-      assert.deepEqual(map([-1,0,-2], increment), [0,1,-1]);
-    });
-  });
-  describe("with decrement", function(){
-    it("should decrement all array elements", function(){
-      assert.deepEqual(map([], decrement), []);
-      assert.deepEqual(map([0], decrement), [-1]);
-      assert.deepEqual(map([1], decrement), [0]);
-      assert.deepEqual(map([-1], decrement), [-2]);
-      assert.deepEqual(map([1,2], decrement), [0,1]);
       assert.deepEqual(map([-1,-2], decrement), [-2,-3]);
-      assert.deepEqual(map([1,0,2], decrement), [0,-1,1]);
       assert.deepEqual(map([-1,0,-2], decrement), [-2,-1,-3]);
-    });
-  });
-  describe("with square",function(){
-    it("should square all array elements", function(){
-      assert.deepEqual(map([1,2], square), [1,4]);
-    });
-  });
-  describe("with cube",function(){
-    it("should cube all array elements", function(){
-      assert.deepEqual(map([1,2], cube), [1,8]);
-    });
+      assert.deepEqual(map([-1,-2], square), [1,4]);
+      assert.deepEqual(map([1,0,2], square), [1,0,4]);
+      assert.deepEqual(map([-1,-2], cube), [-1,-8]);
+      assert.deepEqual(map([-1,0,-2], cube), [-1,0,-8]);
   });
 });
 
 describe("filter", function(){
-  describe("using isEven as callback function", function(){
-    it("should give array containing only even numbers", function(){
+  it("with empty array should return empty array", function(){
       assert.deepEqual(filter([], isEven), []);
-      assert.deepEqual(filter([1], isEven), []);
-      assert.deepEqual(filter([2], isEven), [2]);
-      assert.deepEqual(filter([1,2,3,4], isEven), [2,4]);
-      assert.deepEqual(filter([-1,-2,-3,-4], isEven), [-2,-4]);
-    });
-  });
-  describe("using isOdd as callback function", function(){
-    it("should give array containing only odd numbers", function(){
       assert.deepEqual(filter([], isOdd), []);
+  });
+  it("with array of any arity containing all truthy values should return same array", function(){
+      assert.deepEqual(filter([2], isEven), [2]);
+      assert.deepEqual(filter([2,4], isEven), [2,4]);
       assert.deepEqual(filter([1], isOdd), [1]);
+      assert.deepEqual(filter([1,3], isOdd), [1,3]);
+  });
+  it("with array of any arity containing all falsy values should return same array", function(){
+      assert.deepEqual(filter([1], isEven), []);
+      assert.deepEqual(filter([1,3], isEven), []);
       assert.deepEqual(filter([2], isOdd), []);
-      assert.deepEqual(filter([1,2,3,4], isOdd), [1,3]);
-      assert.deepEqual(filter([-1,-2,-3,-4], isOdd), [-1,-3]);
-    });
+      assert.deepEqual(filter([2,4], isOdd), []);
+  });
+  it("with array containing miscellaneous values should return array of truthy values", function(){
+    assert.deepEqual(filter([1,2,3,4], isEven), [2,4]);
+    assert.deepEqual(filter([1,2,3,4], isOdd), [1,3]);
   });
 });
